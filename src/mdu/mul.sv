ac2bb5fad8f5de0dae07a8d4f817a9f2acf1d455
Roman De Santos <romanspersonalemail@gmail.com>
2025-03-03 20:08:47 -0800

    update

--- src/mdu/mul.sv
+++ src/mdu/mul.sv
@@ -46,40 +46,40 @@ module mul #(parameter XLEN) (
   logic [XLEN-2:0] Aprime, Bprime;
 
   always_comb begin
-  Aprime = ForwardedSrcAE[XLEN-2:0];
-  Am = ForwardedSrcAE[XLEN-1];
-  Bm = ForwardedSrcBE[XLEN-1];
-  Bprime = ForwardedSrcBE[XLEN-2:0];
+    Aprime = ForwardedSrcAE[XLEN-2:0];
+    Am = ForwardedSrcAE[XLEN-1];
+    Bm = ForwardedSrcBE[XLEN-1];
+    Bprime = ForwardedSrcBE[XLEN-2:0];
 
   
     case(Funct3E)
 
       3'b000: begin // MUL
         PP1E = Aprime*Bprime;
-        PP2E = Bm&Aprime;
-        PP3E = Am&Bprime;
-        PP4E = (Am&Bm) << (2*XLEN -2);
+        PP2E = Bm*Aprime;
+        PP3E = Am*Bprime;
+        PP4E = (Am*Bm) << (2*XLEN -2);
       end
 
       3'b001: begin //MULH
         PP1E = Aprime*Bprime;
-        PP2E = ~(Bm&Aprime);
-        PP3E = ~(Am&Bprime);
-        PP4E = ((Am&Bm) << (2*XLEN -2)) + (1 << 2*XLEN-1) + (1 << XLEN);
+        PP2E = ~(Bm*Aprime);
+        PP3E = ~(Am*Bprime);
+        PP4E = ((Am*Bm) << (2*XLEN -2)) + (1 << 2*XLEN-1) + (1 << XLEN);
       end
 
       3'b010: begin //MULHSU
         PP1E = Aprime*Bprime;
-        PP2E = ~(Bm&Aprime);
-        PP3E = ~(Am&Bprime);
-        PP4E = ((Am&Bm) << (2*XLEN -2)) + (1 << 2*XLEN-1) + (1 << XLEN-1);
+        PP2E = ~(Bm*Aprime);
+        PP3E = ~(Am*Bprime);
+        PP4E = ((Am*Bm) << (2*XLEN -2)) + (1 << 2*XLEN-1) + (1 << XLEN-1);
       end
 
       3'b011: begin //MULHU
         PP1E = Aprime*Bprime;
-        PP2E = Bm&Aprime;
-        PP3E = ~(Am&Bprime);
-        PP4E = ((Am&Bm) << (2*XLEN -2));
+        PP2E = Bm*Aprime;
+        PP3E = ~(Am*Bprime);
+        PP4E = ((Am*Bm) << (2*XLEN -2));
       end
 
       default begin
